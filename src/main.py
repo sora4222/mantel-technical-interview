2672a6b531c3ac00c409f0e2ccc261cb3d0e10ed
Jesse <25578668+sora4222@users.noreply.github.com>
2024-11-23 12:31:04 +1100

    refactor(start): move start

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from src.kohonen import train
+import numpy as np
+
+if __name__ == '__main__':
+    # Generate data
+    input_data = np.random.random((10,3))
+    image_data = train(input_data, 100, 10, 10)
+
+    plt.imsave('100.png', image_data)
+
+    # Generate data
+    input_data = np.random.random((10,3))
+    image_data = train(input_data, 1000, 100, 100)
+
+    plt.imsave('1000.png', image_data)
