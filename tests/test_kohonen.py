63366aec86ea205ce311eb12b4efd024950c1e63
Jesse <25578668+sora4222@users.noreply.github.com>
2024-11-24 12:37:58 +1100

    feat(test): add bmu test

---
+++ tests/test_kohonen.py
@@ -0,0 +1,15 @@
+from src.kohonen import Kohonen
+import numpy as np
+
+
+def test_bmu_calculated():
+    input_data = np.array([
+        [0.75, 0.75, 0.75],
+        [0.25, 0.25, 0.25]
+    ])
+    som = Kohonen(input_data, 4, 4)
+    som.weights = np.zeros_like(som.weights)
+    som.weights[0][0] = np.array([0.75, 0.75, 0.75])
+    som.weights[3][3] = np.array([0.25, 0.25, 0.25])
+    bmu = som._best_matching_unit(input_data[0])
+    assert bmu == (0, 0)
